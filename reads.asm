bc109094b95bda265cc4911841c721516c4a62b7
ALAN JOSE <130158432+Alanj7788@users.noreply.github.com>
2023-12-04 19:27:16 +0530

    Create reads.asm

---
+++ reads.asm
@@ -0,0 +1,34 @@
+;Taking input string and printing it
+.model small
+
+printn macro p
+    lea dx, p
+    mov ah, 09h
+    int 21h
+endm
+
+.data
+    buffer db 40 ; 
+    msg1 db 'Enter a string:$'
+    msg2 db 10,13,'You entered:$'
+
+.code
+    mov ax, @data
+    mov ds, ax
+
+    printn msg1
+    
+    lea dx, buffer  
+    mov ah, 0Ah    
+    int 21h       
+  
+    printn msg2
+ 
+    ; Print the entered string (use the buffer directly)
+    lea dx, buffer+2
+    mov ah, 09h
+    int 21h
+
+    mov ah, 4ch
+    int 21h
+end
